6ea436916e1fd72c0374b50f08ef36e858bed491
cod-lvr <aymang29@yahoo.com>
2021-05-22 04:32:56 +0200

    add new style sheet

--- src/components/expenses/ExpenseDate.js
+++ src/components/expenses/ExpenseDate.js
@@ -2,14 +2,14 @@ import Card from '../UI/Card';
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-    const mounth = props.date.toLocaleDateString("en-US", { month: 'long'});
+    const month = props.date.toLocaleDateString("en-US", { month: 'long'});
     const day = props.date.toLocaleDateString("en-US", { day: '2-digit'});
     const year = props.date.getFullYear();
     return (
-        <Card className="expense">
-            <div>{day}</div>
-            <div>{mounth}</div>
-            <div>{year}</div>
+        <Card className="expense-date">
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
         </Card>
     );
 }
