f10758d1d1655b1fbecb50b365f753fbe1d5914f
cod-lvr <aymang29@yahoo.com>
2021-05-24 23:51:39 +0200

    add expensesList

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './ExpenseItem.css';
-import './ExpenseDate';
-import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+import "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">  
+    <li>
+      <div className="expense-item">
         <div className="expense-item__description">
-            <ExpenseDate date={props.date}/>
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+          <ExpenseDate date={props.date} />
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
-    </div> 
+      </div>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
